a23ccd5d4f1d2cf16ab048f37dea0a6753c32764
will-cotton4 <will.cotton4@gmail.com>
2019-09-21 18:26:41 -0500

    removed placeholder/demo text

--- fifth-native-app/navigation/MainTabNavigator.js
+++ fifth-native-app/navigation/MainTabNavigator.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
-
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import RoutineScreen from '../screens/RoutineScreen';
+
 
 const config = Platform.select({
   web: { headerMode: 'screen', },
