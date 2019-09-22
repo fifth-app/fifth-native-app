9975e385beb4a2f3f1acebee89f7dd5fa328442c
angel-torres <alas4224@gmail.com>
2019-09-22 10:27:12 -0500

    updated routine screen

--- fifth-native-app/screens/RoutineScreen.js
+++ fifth-native-app/screens/RoutineScreen.js
@@ -4,9 +4,12 @@ import { ScrollView, StyleSheet, Text, Button } from 'react-native';
 var moves = {
   'fifth_rest_state': {
     'next_moves': [
-      'tendu_basic'],
+      'tendu_basic',
+      'degage',
+      'grand_battement'],
     'official_name': 'Fifth'
     ,
+    'poss_starting': false
   },
   'tendu_basic': {
     'next_moves': [
@@ -14,39 +17,66 @@ var moves = {
       'tendu_down',
       'tendu_in',
       'fifth_rest_state'],
-    'official_name': 'Tendu'
+    'official_name': 'Tendu',
+    'poss_starting': true
   },
   'tendu_flex': {
     'next_moves': [
-      'tendu_basic'],
-    'official_name': 'Flex'
+      'tendu_basic',
+      'degage',
+      'grand_battement'],
+    'official_name': 'Flex',
+    'poss_starting': false
 
   },
   'tendu_down': {
     'next_moves': [
-      'tendu_basic'],
-    'official_name': 'Foot down'
+      'tendu_basic',
+      'degage',
+      'grand_battement'],
+    'official_name': 'Foot down',
+    'poss_starting': false
   },
   'tendu_in': {
     'next_moves': [
-      'tendu_basic'],
-    'official_name': 'Turn in'
+      'tendu_basic',
+      'degage',
+      'grand_battement'],
+    'official_name': 'Turn in',
+    'poss_starting': false
+  },
+  'degage': {
+    'next_moves': [
+      'tendu_basic',
+      'degage',
+      'grand_battement'],
+    'official_name': 'Degage',
+    'poss_starting': true
+  },
+  'grand_battement': {
+    'next_moves': [
+      'tendu_basic',
+      'degage',
+      'grand_battement'],
+    'official_name': 'Grand battement',
+    'poss_starting': true
   }
 }
 
 export default function RoutineScreen() {
-  const movesArray = Object.values(moves);
+  const movesArray = Object.keys(moves);
   const [currentMove, setCurrentMove] = useState('');
+
+  // const handleClick = {
+  //   // console.log("hello")
+  // }
 
   return (
     <ScrollView style={styles.container}>
-      <Button title="Tendu" onPress={setCurrentMove("Tendu")} />
-
-      currentMove ===
       {
         movesArray.map((move, index) => {
-          console.log(movesArray[index]);
-          return <Button title={movesArray[index].official_name} key={index} />
+          console.log(moves[move].poss_starting)
+          return moves[move].poss_starting ? <Button key={index} title={move} onPress={() => setCurrentMove(move)} /> : null;
         })
       }
     </ScrollView>
